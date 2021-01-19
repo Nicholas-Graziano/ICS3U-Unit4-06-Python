b5dad3a38251fc74481aaa55cda5ebbcc200e8dd
Ubuntu <ubuntu@ip-172-31-9-20.ec2.internal>
2021-01-19 21:26:20 +0000

    added RGB nested loop

---
+++ RGB_nested_loops.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by: Nicholas Graziano
+# Created on: November 2020
+# This program checks if a number is negative or positive
+
+def main():
+    # input
+ 
+    # process and output
+        for loop_counter1 in range(256):
+            for loop_counter2 in range(256):
+                for loop_counter3 in range(256):
+                    print(loop_counter1, loop_counter2, loop_counter3)
+
+
+
+
+if __name__ == "__main__":
+    main()
